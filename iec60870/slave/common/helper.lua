6bf6ab0c3e6e895b4e3865bbb3ece09af22d2da4
Dirk Chang <dirk@kooiot.com>
2023-08-30 16:12:26 +0800

    helper do not need to be class

--- iec60870/slave/common/helper.lua
+++ iec60870/slave/common/helper.lua
@@ -4,16 +4,16 @@ local ti_map = require 'iec60870.asdu.ti_map'
 local asdu_addr = require 'iec60870.asdu.addr'
 local asdu_object = require 'iec60870.asdu.object'
 
-local helper = class('LUA_IEC60870_SLAVE_COMMON_HELPER')
+local _M = {}
 
-function helper:initialize()
+function _M.initialize()
 end
 
-function helper:make_object(ti, addr, ...)
+function _M.make_object(ti, addr, ...)
 	return asdu_object:new(ti, asdu_addr:new(addr), { ... })
 end
 
-function helper:make_cp24time2a(timestamp)
+function _M.make_cp24time2a(timestamp)
 	local ms = timestamp % 1000
 	local sec = (timestamp // 1000) % 60
 	local min = (timestamp // 60000) % 60
@@ -25,8 +25,8 @@ end
 -- val: number(0 or 1)
 -- timestamp: number
 --]]
-function helper:make_sp_na(addr, val)
-	return self:make_object(types.M_SP_NA_1, addr, ti_map.create_data('SIQ', val))
+function _M.make_sp_na(addr, val)
+	return _M.make_object(types.M_SP_NA_1, addr, ti_map.create_data('SIQ', val))
 end
 
 --[[
@@ -34,46 +34,46 @@ end
 -- val: number(0 or 1)
 -- timestamp: number
 --]]
-function helper:make_sp_ta(addr, val, timestamp)
-	local t = self:make_cp24time2a(timestamp)
-	return self:make_object(types.M_SP_TA_1, addr, ti_map.create_data('SIQ', val), t)
+function _M.make_sp_ta(addr, val, timestamp)
+	local t = _M.make_cp24time2a(timestamp)
+	return _M.make_object(types.M_SP_TA_1, addr, ti_map.create_data('SIQ', val), t)
 end
 
-function helper:make_dp_na(addr, val)
-	return self:make_object(types.M_DP_NA_1, addr, ti_map.create_data('DIQ', val))
+function _M.make_dp_na(addr, val)
+	return _M.make_object(types.M_DP_NA_1, addr, ti_map.create_data('DIQ', val))
 end
 
-function helper:make_dp_ta(addr, val, timestamp)
-	local t = self:make_cp24time2a(timestamp)
-	return self:make_object(types.M_DP_TA_1, addr, ti_map.create_data('DIQ', val))
+function _M.make_dp_ta(addr, val, timestamp)
+	local t = _M.make_cp24time2a(timestamp)
+	return _M.make_object(types.M_DP_TA_1, addr, ti_map.create_data('DIQ', val))
 end
 
-function helper:make_me_na(addr, val)
-	return self:make_object(types.M_ME_NA_1, addr, ti_map.create_data('NVA', val), ti_map.create_data('QDS'))
+function _M.make_me_na(addr, val)
+	return _M.make_object(types.M_ME_NA_1, addr, ti_map.create_data('NVA', val), ti_map.create_data('QDS'))
 end
 
-function helper:make_me_ta(addr, val, timestamp)
-	local t = self:make_cp24time2a(timestamp)
-	return self:make_object(types.M_ME_TA_1, addr, ti_map.create_data('NVA', val), ti_map.create_data('QDS'), t)
+function _M.make_me_ta(addr, val, timestamp)
+	local t = _M.make_cp24time2a(timestamp)
+	return _M.make_object(types.M_ME_TA_1, addr, ti_map.create_data('NVA', val), ti_map.create_data('QDS'), t)
 end
 
-function helper:make_me_nb(addr, val)
-	return self:make_object(types.M_ME_NA_1, addr, ti_map.create_data('SVA', val), ti_map.create_data('QDS'))
+function _M.make_me_nb(addr, val)
+	return _M.make_object(types.M_ME_NA_1, addr, ti_map.create_data('SVA', val), ti_map.create_data('QDS'))
 end
 
-function helper:make_me_tb(addr, val, timestamp)
-	local t = self:make_cp24time2a(timestamp)
-	return self:make_object(types.M_ME_TA_1, addr, ti_map.create_data('SVA', val), ti_map.create_data('QDS'), t)
+function _M.make_me_tb(addr, val, timestamp)
+	local t = _M.make_cp24time2a(timestamp)
+	return _M.make_object(types.M_ME_TA_1, addr, ti_map.create_data('SVA', val), ti_map.create_data('QDS'), t)
 end
 
 -- SQ is the addr index
-function helper:make_it_na(addr, val, sq)
-	return self:make_object(types.M_IT_NA_1, addr, ti_map.create_data('BCR', val, sq))
+function _M.make_it_na(addr, val, sq)
+	return _M.make_object(types.M_IT_NA_1, addr, ti_map.create_data('BCR', val, sq))
 end
 
-function helper:make_it_ta(addr, val, sq, timestamp)
-	local t = self:make_cp24time2a(timestamp)
-	return self:make_object(types.M_IT_TA_1, addr, ti_map.create_data('BCR', val, sq), t)
+function _M.make_it_ta(addr, val, sq, timestamp)
+	local t = _M.make_cp24time2a(timestamp)
+	return _M.make_object(types.M_IT_TA_1, addr, ti_map.create_data('BCR', val, sq), t)
 end
 
-return helper
+return _M
